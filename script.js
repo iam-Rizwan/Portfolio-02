f350d0f6201b32fdced91ef21a5438043094cf07
iam-Rizwan <amohammedrizwan2001@gmail.com>
2025-08-17 16:56:23 +0530

    commit

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     });
+   
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
 
     const observerOptions = {
         root: null,
